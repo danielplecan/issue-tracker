c70d101d9e4325d62be12d1123402a9240cd0cca
enatataru <atataru@EN60675.endava.net>
2014-07-16 18:09:20 +0300

    First method of the Issue service

--- src/main/java/internship/issuetracker/service/IssueService.java
+++ src/main/java/internship/issuetracker/service/IssueService.java
@@ -6,7 +6,11 @@
 
 package internship.issuetracker.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
+import internship.issuetracker.entity.User;
+import internship.issuetracker.entity.Issue;
 
 /**
  *
@@ -15,5 +19,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class IssueService {
+    @PersistenceContext
+    private EntityManager em;
     
+    /**
+     * Create a new issue and persist it in the database;
+     * @param title - the title of post
+     * @param content - the content of post(can be null)
+     * @param user  - the user who created the post
+     */
+    public Issue createIssue(String title, String content, User user){
+        return new Issue();
+    }
 }
