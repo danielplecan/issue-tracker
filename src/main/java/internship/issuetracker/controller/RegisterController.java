aa23aba42c2516433016b069ffac3ecc7b8362be
irinel.nistor <irinel.nistor@endava.com>
2014-07-17 09:19:26 +0300

    Created register controller

---
+++ src/main/java/internship/issuetracker/controller/RegisterController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package internship.issuetracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RegisterController {
+
+    @RequestMapping(value = {"/register"},method = RequestMethod.GET)
+    public String registerUser() {
+        return "register";
+    }
+}
