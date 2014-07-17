ef872c23d3db1ddde91017b0790523d8a470ff2c
danielplecan <daniel.plecan@gmail.com>
2014-07-17 11:50:08 +0300

    Modified UserService to support registration, checking for username and email existence

--- src/main/java/internship/issuetracker/service/UserService.java
+++ src/main/java/internship/issuetracker/service/UserService.java
@@ -6,10 +6,66 @@
 
 package internship.issuetracker.service;
 
+import internship.issuetracker.dto.UserDTO;
+import internship.issuetracker.entity.User;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author dplecan
  */
 public class UserService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void registerUser(UserDTO userDTO) {
+        User user = getUserFromDTO(userDTO);
+
+        entityManager.persist(user);
+    }
+
+    @Transactional
+    public boolean checkUsernameExistence(String username) {
+        TypedQuery<User> userQuery = entityManager.createNamedQuery(User.FIND_BY_USERNAME, User.class);
+        userQuery.setParameter("v_username", username);
+
+        List<User> resultList = userQuery.getResultList();
+
+        return resultList != null && !resultList.isEmpty();
+    }
+
+    @Transactional
+    public boolean checkEmailExistence(String email) {
+        TypedQuery<User> userQuery = entityManager.createNamedQuery(User.FIND_BY_EMAIL, User.class);
+        userQuery.setParameter("v_email", email);
+
+        List<User> resultList = userQuery.getResultList();
+
+        return resultList != null && !resultList.isEmpty();
+    }
     
+    public String encryptPassword(String password) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(password);
+    }
+
+    public User getUserFromDTO(UserDTO userDTO) {
+        User user = new User();
+
+        user.setName(userDTO.getName());
+        user.setUsername(userDTO.getUsername());
+        user.setEmail(userDTO.getEmail());
+        user.setPasswordHash(encryptPassword(userDTO.getPassword()));
+
+        return user;
+    }
 }
