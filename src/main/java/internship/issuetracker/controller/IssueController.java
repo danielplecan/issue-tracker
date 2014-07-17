51f7d620b8eeea16e7bb7c23ed9c64ba8c1c89a2
ionut.apavaloaie <iapavaloiae@gmail.com>
2014-07-17 16:21:28 +0300

    added view+controller for all isuues

--- src/main/java/internship/issuetracker/controller/IssueController.java
+++ src/main/java/internship/issuetracker/controller/IssueController.java
@@ -1,4 +1,3 @@
-
 package internship.issuetracker.controller;
 
 import internship.issuetracker.entity.Issue;
@@ -6,6 +5,7 @@ import internship.issuetracker.entity.IssueState;
 import internship.issuetracker.entity.User;
 import internship.issuetracker.service.IssueService;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,44 +23,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  * @author scalin
  */
-
 //@Service
 @Controller
 public class IssueController {
-    
+
     @Autowired
     IssueService issueService;
-    
+
     @PersistenceContext
     EntityManager em;
 
     @RequestMapping(value = "/issue/{id}", method = RequestMethod.GET)
-    public String viewIssue(@PathVariable String id, Model model){
-        
+    public String viewIssue(@PathVariable String id, Model model) {
+
         Long issueId = Long.parseLong(id);
         Issue result = issueService.getIssueById(issueId);
         model.addAttribute("issue", result);
-        
+
         return "issue";
     }
-    
-   @RequestMapping(value = {"/create-issue"},method = RequestMethod.GET)
+
+    @RequestMapping(value = {"/create-issue"}, method = RequestMethod.GET)
     public String createIssue(Model model) {
-        model.addAttribute("issue",new Issue());
+        model.addAttribute("issue", new Issue());
         return "create-issue";
     }
-    
-     @RequestMapping(value = {"/create-issue"},method = RequestMethod.POST)
-    public String createIssue(@ModelAttribute("issue")Issue issue,BindingResult bindingResult) {
+
+    @RequestMapping(value = {"/create-issue"}, method = RequestMethod.POST)
+    public String createIssue(@ModelAttribute("issue") Issue issue, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "create-issue";
         }
-        String a= issue.getTitle();
+        String a = issue.getTitle();
         System.out.println(issue.getTitle());
-        
-        
+
         //until we can actually have a current user
-        User u = em.find(User.class, (long)1);
+        User u = em.find(User.class, (long) 1);
         issue.setOwner(u);
         Date data = new Date();
         issue.setDate(data);
@@ -69,7 +67,7 @@ public class IssueController {
         issueService.createIssue(issue);
         return "home";
     }
-    
+
     public void setIssueService(IssueService issueService) {
         this.issueService = issueService;
     }
@@ -77,22 +75,31 @@ public class IssueController {
     public IssueService getIssueService() {
         return issueService;
     }
-    
+
     @RequestMapping(method = RequestMethod.POST, value = "/issue/{id}/open")
     public ResponseEntity<Issue> openIssue(@PathVariable("id") int id) {
-           System.out.println(id);
-           return new ResponseEntity<>(HttpStatus.OK);
+        System.out.println(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-    
+
     @RequestMapping(method = RequestMethod.POST, value = "/issue/{id}/close")
     public ResponseEntity<Issue> closeIssue(@PathVariable("id") int id) {
-           System.out.println(id);
-           return new ResponseEntity<>(HttpStatus.OK);
+        System.out.println(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
- 
+
     @RequestMapping(method = RequestMethod.POST, value = "/issue/{id}/reopen")
     public ResponseEntity<Issue> reopenIssue(@PathVariable("id") int id) {
-           System.out.println(id); 
-           return new ResponseEntity<>(HttpStatus.OK);
+        System.out.println(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/issues", method = RequestMethod.GET)
+    public String viewAllIssues(Model model) {
+
+        List<Issue> issues = issueService.getIssues();
+        model.addAttribute("issues", issues);
+
+        return "issues";
     }
 }
