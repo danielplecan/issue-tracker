2bcebafca1cad0321500d2d0f5c12806081d3c28
danielplecan <daniel.plecan@gmail.com>
2014-07-17 09:56:18 +0300

    Created User Entity

--- src/main/java/internship/issuetracker/entity/User.java
+++ src/main/java/internship/issuetracker/entity/User.java
@@ -6,10 +6,76 @@
 
 package internship.issuetracker.entity;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
  *
  * @author dplecan
  */
-public class User {
+@Entity
+@Table(name = "en_users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "en_users_id_seq", sequenceName = "en_users_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="username", nullable = false)
+    private String username;
+
+    @Column(name="email", nullable = false)
+    private String email;
     
+    @Column(name="password_hash", length = 60, nullable = false)
+    private String passwordHash;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
 }
