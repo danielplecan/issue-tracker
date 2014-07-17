77972ff748c62ba854c205402aec0fe0d5e27179
danielplecan <daniel.plecan@gmail.com>
2014-07-17 16:55:26 +0300

    Login functionality implemented

--- src/main/java/internship/issuetracker/controller/LoginController.java
+++ src/main/java/internship/issuetracker/controller/LoginController.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package internship.issuetracker.controller;
 
-import internship.issuetracker.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +10,6 @@ public class LoginController {
 
     @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
     public String loginUser(Model model) {
-        model.addAttribute("user", new UserDTO());
         return "login";
     }
-
-    @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
-    public String loginPost(Model model) {
-        model.addAttribute("user", new UserDTO());
-        return "home";
-    }
-
 }
--- src/main/java/internship/issuetracker/entity/User.java
+++ src/main/java/internship/issuetracker/entity/User.java
@@ -40,10 +40,10 @@ public class User implements Serializable {
     @Column(name="name", nullable = false)
     private String name;
 
-    @Column(name="username", nullable = false)
+    @Column(name="username", nullable = false, unique = true)
     private String username;
 
-    @Column(name="email", nullable = false)
+    @Column(name="email", nullable = false, unique = true)
     private String email;
     
     @Column(name="password_hash", length = 60, nullable = false)
