0fbe04b0c5c5d0c903aad05a20f3d15527e3e360
enatataru <atataru@EN60675.endava.net>
2014-07-18 13:37:22 +0300

    One button

--- src/main/webapp/resources/js/changeStateModule.js
+++ src/main/webapp/resources/js/changeStateModule.js
@@ -24,6 +24,33 @@ var openStateToggle = function(button, label) {
             });
     });
 };
+
+var openStateToggle = function(button, label) {
+    $(button).click( function(){
+        console.log('click');
+        var action = 'reopen';
+        var issueId = $(label).attr('data-id');
+        var url = 'http://localhost:8080/issue-tracker/issue/' + issueId + '/' + action;
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                   if( data.status == 'succes') {
+                       $(label).text('REOPENED');
+                       $(label).removeClass('label-danger');
+                       $(label).addClass('label-warning');
+                       $('.toggle').hide();
+                       $('#changeState-close').show();
+                   } else {
+                       console.log('fail');
+                   }
+            }
+            });
+    });
+};
 (function (){
     var button = $('#changeState-reopen');
     var span = $('#issueState');
