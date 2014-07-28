093d6d15cc9bb4122b284c83802ceb7a00592ba7
ionut.apavaloaie <iapavaloiae@gmail.com>
2014-07-28 14:44:04 +0300

    View all issues refactor + added content preview

--- src/main/webapp/resources/js/issuesCreator.js
+++ src/main/webapp/resources/js/issuesCreator.js
@@ -5,15 +5,15 @@ var issuesCreator = function() {
         switch (state) {
             case "OPEN":
                 stateLabel = $("<div>Open</div>");
-                stateLabel.addClass("label label-success issueLength stateLabelIssues");
+                stateLabel.addClass("label label-success theIssueState");
                 break;
             case "CLOSED":
                 stateLabel = $("<div>Closed</div>");
-                stateLabel.addClass("label label-danger issueLength stateLabelIssues");
+                stateLabel.addClass("label label-danger theIssueState");
                 break;
             case "REOPENED":
                 stateLabel = $("<div>Reopened</div>");
-                stateLabel.addClass("label label-warning issueLength stateLabelIssues");
+                stateLabel.addClass("label label-warning theIssueState");
                 break;
         }
         return stateLabel;
@@ -21,61 +21,106 @@ var issuesCreator = function() {
     var createIssueTitle = function(title, id) {
         var titleLabel = $("<a></a>");
         titleLabel.text(title);
-        titleLabel.addClass("titleLink");
+        titleLabel.addClass("theIssueTitleLink");
         titleLabel.attr("href", "/issue/" + id);
         return titleLabel;
     };
-    var createIssue = function(issue) {
-        var issueTitle = $("<div></div>");
-        issueTitle.addClass("issueTitle");
-        issueTitle.append(createIssueState(issue.state));
-        issueTitle.append(createIssueTitle(issue.title, issue.id));
-        return issueTitle;
+
+    var createContentDiv = function(issue) {
+
+        var issueContentDiv = $("<div/>");
+        issueContentDiv.addClass("theIssueContent");
+
+        var issueContentPreview = $("<div/>");
+        issueContentPreview.addClass("theIssueContentPreview hidden");
+        issueContentPreview.attr("data-contentId", issue.id);
+        issueContentPreview.text(issue.content);
+
+        issueContentDiv.append(issueContentPreview);
+        issueContentDiv.append(createIssueLabelsDiv(issue.labels));
+
+        return issueContentDiv;
+
     };
-    var createLabelDiv = function(labels) {
+
+    var createIssueLabelsDiv = function(labels) {
         var labelsDiv = $("<div/>");
-        if(labels===null)
+        labelsDiv.addClass("theIssueLabels");
+        if (labels === null)
             return labelsDiv;
-        $.each(labels, function(index,label) {
+        $.each(labels, function(index, label) {
             var spanLabel = $("<span/>");
-            spanLabel.addClass("label");
-            spanLabel.css("margin-left","2px");
-            spanLabel.css("background-color",label.color);
-            spanLabel.css("color",getContrastYIQ(label.color));
+            spanLabel.addClass("theIssueLabel label");
+            spanLabel.css("background-color", label.color);
+            spanLabel.css("color", getContrastYIQ(label.color));
             spanLabel.text(label.name);
             labelsDiv.append(spanLabel);
         });
         return labelsDiv;
+    }
+
+    var createPostedDetailsSpan = function(text) {
+        var span = $("<span></span>");
+        span.text(text);
+        span.addClass("text-primary");
+        return span;
     };
-    var createDateSpan = function(text) {
-        var label = $("<span></span>");
-        label.text(text);
-        label.addClass("text-primary");
-        return label;
-    };
-    var createDateTimeUpdate = function(issue) {
-        var dateTimeUpdate = $("<div></div>");
-        dateTimeUpdate.addClass("issueDateTimeUpdate");
+    var createIssueLeftDetails = function(issue) {
+        var dateTimeUpdate = $("<span>");
+        dateTimeUpdate.addClass("theIssueLeftDetails");
         dateTimeUpdate.append("<i>Posted by </i>");
-        dateTimeUpdate.append(createDateSpan(issue.owner.name));
+
+        dateTimeUpdate.append(createPostedDetailsSpan(issue.owner.name));
         dateTimeUpdate.append("<i> on </i>");
-        dateTimeUpdate.append(createDateSpan(issue.dateFormat));
+        dateTimeUpdate.append(createPostedDetailsSpan(issue.dateFormat));
         return dateTimeUpdate;
     };
-    var createIssueDateTime = function(issue) {
-        var issueDateTime = $("<div></div>");
-        issueDateTime.addClass("issueDateTime");
-        issueDateTime.append("<i>Last updated </i>");
-        issueDateTime.append(createDateSpan(issue.lastUpdateDate));
+    var createIssueRightDetails = function(issue) {
+        var issueDateTime = $("<span/>");
+        issueDateTime.addClass("theIssueRightDetails");
+        issueDateTime.append("<i>Last updated by </i>");
+        issueDateTime.append(createPostedDetailsSpan("LAST AUTHOR!!!"));
+        issueDateTime.append("<i>, </i>");
+        issueDateTime.append(createPostedDetailsSpan(issue.lastUpdateDate));
         issueDateTime.append("<i> ago</i>");
         return issueDateTime;
     };
-    var createDates = function(issue) {
-        var dates = $("<div></div>");
-        dates.addClass("dates");
-        dates.append(createDateTimeUpdate(issue));
-        dates.append(createIssueDateTime(issue));
+    var createIssueDetailsDiv = function(issue) {
+        var dates = $("<div/>");
+        dates.addClass("theIssueDetails");
+
+        dates.append(createIssueLeftDetails(issue));
+        dates.append(createIssueRightDetails(issue));
         return dates;
+    };
+
+    var createShowContentButton = function(issueId) {
+        var span = $("<span/>");
+        span.attr("title", "Show Issue Content");
+        span.addClass("theIssueShowContent glyphicon glyphicon-chevron-down");
+        span.attr("data-issueid", issueId);
+        span.click(function() {
+            $this = $(this);
+            $this.toggleClass("glyphicon glyphicon-chevron-down");
+            $this.toggleClass("glyphicon glyphicon-chevron-up");
+
+
+            var selector = "div[data-contentId=" + $this.data().issueid + "]";
+            $(selector).toggleClass("hidden");
+        });
+        return span;
+    };
+
+    var createTitleDiv = function(issue) {
+        var titleDiv = $("<div/>");
+        titleDiv.addClass("theIssueTitle");
+
+        titleDiv.append(createShowContentButton(issue.id));
+        titleDiv.append(createIssueState(issue.state));
+        titleDiv.append(createIssueTitle(issue.title, issue.id));
+
+        return titleDiv;
+
     };
     var noResultMessage = function(text) {
         var noResultLabel = $("<p/>");
@@ -85,26 +130,21 @@ var issuesCreator = function() {
     };
     var addIssue = function(issue) {
         var outerDiv = $("<div/>");
-        outerDiv.addClass("firstRow");
+        outerDiv.addClass("theIssue col-xs-offset-1 col-xs-10 panel panel-default");
         outerDiv.attr("data-id", issue.id);
 
-        var innerDiv = $("<div/>");
-        innerDiv.addClass("panel panel-default col-lg-offset-1 col-lg-10 noBorder");
+        outerDiv.append(createTitleDiv(issue));
+        outerDiv.append(createContentDiv(issue));
+        outerDiv.append(createIssueDetailsDiv(issue));
 
-        outerDiv.append(innerDiv);
-        var titleDiv = $("<div></div>");
-        titleDiv.append(createIssue(issue));
-        allIssuesDiv.append(outerDiv);
-        innerDiv.append(titleDiv);
-        innerDiv.append(createLabelDiv(issue.labels));
-        innerDiv.append(createDates(issue));
+        return outerDiv;
     };
     return{
-        addAllIssues: function(issues,totalResult) {
+        addAllIssues: function(issues, totalResult) {
             allIssuesDiv = $("#allIssues");
             allIssuesDiv.empty();
             $.each(issues, function(index, item) {
-                addIssue(item);
+                allIssuesDiv.append(addIssue(item));
             });
             noResultMessage(totalResult <= 0 ? "No results were found" : totalResult + " in total");
         }
