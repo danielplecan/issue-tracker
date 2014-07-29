bdf098e94ad088f2c72f5c4fd25b27d70d4df255
danielplecan <daniel.plecan@gmail.com>
2014-07-29 20:08:53 +0300

    Basic uploading functionality, including progress bar

---
+++ src/main/webapp/resources/js/upload.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo(document.body);
+            });
+        },
+        progressall: function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('.progress-bar').css(
+                'width',
+                progress + '%'
+            );
+        },
+        maxFileSize: 1,
+        processfail: function(e, data) {
+            alert(data.files[data.index].error);
+            console.log(data);
+        }
+    });
+});
