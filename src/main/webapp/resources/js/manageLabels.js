2057f37c93f65a2479eb72994ab6ac2854dfd940
enatataru <atataru@EN60675.endava.net>
2014-07-30 17:38:13 +0300

    Added script escape in view. Now you can add labels(errors are not cheked yet).

--- src/main/webapp/resources/js/manageLabels.js
+++ src/main/webapp/resources/js/manageLabels.js
@@ -1,28 +1,96 @@
-function addFunctionalityToTopPanel(topPanel){
+function createLabelListElement(label) {
+    var li = $('<li class=\"list-group-item labelListThing\">\r\n\n\
+                <div class=\"labelPanel  editLabelPane\">\r\n\n\
+                    <div class=\"showLabelPane\" data-id=\"' + label.id + '\" data-color=\"' + label.color + '\">\r\n\n\
+                        <div class=\"navbar-left\">\r\n\n\
+                            <form class=\"navbar-form navbar-left labelListEdit\">\r\n\n\
+                                <span class=\"labelName theLabelListLabel label theLabelListText\" style=\"background-color: ' + label.color + '\"><\/span>\r\n\n\
+                            <\/form>\r\n \n\
+                       <\/div>\r\n\n\
+                        <div class=\"navbar-right\">\r\n\n\
+                            <button type=\"button\" class=\"btn btn-default btn-edit manageLabelsButton btn-sm\"><span class=\"glyphicon glyphicon-pencil\"><\/span> Edit<\/button>\r\n\n\
+                            <button type=\"button\" class=\"btn btn-default btn-remove manageLabelsButton btn-sm\"><span class=\"glyphicon glyphicon-remove\"><\/span> Delete<\/button>\r\n\n\
+                        <\/div>\r\n\n\
+                    <\/div>\r\n\n\
+                            \r\n\n\
+                    <div class=\"editLabelPane hidden\">\r\n \n\
+                       <div class=\"navbar-left \">\r\n\n\
+                            <div class=\"navbar-form navbar-left labelListEdit\">\r\n\n\
+                                <input type=\"text\" class=\"small-input-box form-control flLeft labelListEdit\" placeholder=\"label\">\r\n\n\
+                                <button class=\"toggle-color-picker color-chooser-color labelColorManageColors col-lg-1\"><\/button>\r\n\n\
+                                <div class=\"theColorsList\" style=\"display: none\">\r\n\n\
+                                    <span class=\"color-chooser-color color-square\" data-color=\"#FF8F8F\" style=\"background-color:#FF8F8F\"><\/span>\r\n\n\
+                                    <span class=\"color-chooser-color color-square\" data-color=\"#FFC69E\" style=\"background-color:#FFC69E\"><\/span>\r\n\n\
+                                    <span class=\"color-chooser-color color-square\" data-color=\"#FFF4C4\" style=\"background-color:#FFF4C4\"><\/span>\r\n\n\
+                                    <span class=\"color-chooser-color color-square\" data-color=\"#E6FAFF\" style=\"background-color:#E6FAFF\"><\/span>\r\n\n\
+                                    <span class=\"color-chooser-color color-square\" data-color=\"#D8FFC4\" style=\"background-color:#D8FFC4\"><\/span>\r\n\n\
+                                    <span class=\"color-chooser-color color-square\" data-color=\"#E6E6E6\" style=\"background-color:#E6E6E6\"><\/span>\r\n \n\
+                                   <span class=\"color-chooser-color color-square\" data-color=\"#B6BDCC\" style=\"background-color:#B6BDCC\"><\/span>\r\n\n\
+                                <\/div>\r\n\n\
+                            <\/div>\r\n\n\
+                        <\/div>\r\n\n\
+                        <div class=\"navbar-right\">\r\n\n\
+                            <button type=\"button\" class=\"btn btn-success btn-sm manageButton btn-save-edit-label\"><span class=\"glyphicon glyphicon-ok-circle\"><\/span> Save<\/button>\r\n\n\
+                            <button type=\"button\" class=\"btn btn-danger btn-sm manageButton btn-cancel-edit-label\"><span class=\"glyphicon glyphicon-remove-circle\"><\/span> Cancel<\/button>\r\n\n\
+                        <\/div>\r\n\n\
+                        <div class=\"col-lg-12 errorMessageManageLabels commentError text-warning commentContent\">Error message<\/div>\r\n\n\
+                    <\/div>\r\n\n\
+                <\/div>\r\n\n\
+            <\/li>');
+    $(li).find('.labelName').text(label.name);
+    return li;
+}
+
+
+function addFunctionalityToTopPanel(topPanel) {
     var newLabelButton = $(topPanel).find('.btn-new-label').first();
     var editLabelPanel = $(topPanel).find('.editLabelPane').first();
     var createLabelButton = $(editLabelPanel).find('.btn-create-edit-label').first();
     var cancelButton = $(editLabelPanel).find('.btn-cancel-edit-label').first();
     var labelNameBox = $(editLabelPanel).find('.small-input-box').first();
-    
+    var toggleColorPickerEdit = $(editLabelPanel).find('.toggle-color-picker').first();
+    var colorList = $(editLabelPanel).find('.theColorsList').first();
+
     function clearInput() {
         $(labelNameBox).val("");
-        $(createLabelButton).attr('disabled', false);
+        $(colorList).hide();
+        $(createLabelButton).attr('disabled', true);
     }
-    
-    $(newLabelButton).click(function () { 
+
+    $(colorList).delegate('.color-square', 'click', function() {
+        $(toggleColorPickerEdit).css('background-color', $(this).attr('data-color'));
+        $(toggleColorPickerEdit).attr('data-color', $(this).attr('data-color'));
+    });
+
+    $(toggleColorPickerEdit).click(function() {
+        colorList.toggle('hidden');
+    });
+
+    $(newLabelButton).click(function() {
+        clearInput();
         $(editLabelPanel).toggle('hidden');
     });
-    
+
     $(createLabelButton).click(function() {
-        
+        var newLabelName = $(labelNameBox).val();
+        var newLabelColor = $(toggleColorPickerEdit).attr('data-color');
+        issueTrackerService.createLabel(newLabelName, newLabelColor).done(function(data) {
+            if (data.success) {
+                var newLabelPanel = $(createLabelListElement(data.label));
+                $(topPanel).siblings('ul.list-group').append(newLabelPanel);
+                clearInput();
+                $(editLabelPanel).toggle('hidden');
+            } else {
+            }
+        });
+
     });
-    
+
     $(cancelButton).click(function() {
         $(editLabelPanel).toggle('hidden');
         clearInput();
     });
-    
+
     $(labelNameBox).on('input', function() {
         var newLabelName = $(this).val();
 
@@ -37,7 +105,7 @@ function addFunctionalityToTopPanel(topPanel){
 $(document).ready(function() {
 
     addFunctionalityToTopPanel($('.manageLabelsTopPanel'));
-    
+
     $('#widgetContainer').delegate('ul>li>.labelPanel', 'click', function(event) {
         var listElementForDelete = $(this).parent();
         var showLabelPanel = $(this).find('.showLabelPane').first();
@@ -60,7 +128,7 @@ $(document).ready(function() {
             $(smallInputBoxEdit).val($(labelNameShow).text());
             $(toggleColorPickerEdit).css('background-color', $(showLabelPanel).attr('data-color'));
             $(toggleColorPickerEdit).attr('data-color', $(showLabelPanel).attr('data-color'));
-            
+
             $(editLabelPanel).removeClass('hidden');
             $(showLabelPanel).hide();
             $(editLabelPanel).show();
