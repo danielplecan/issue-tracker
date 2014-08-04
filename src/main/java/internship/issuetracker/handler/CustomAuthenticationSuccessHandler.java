77d20bf265eeba68734718b998c0808b3a000545
danielplecan <daniel.plecan@gmail.com>
2014-08-04 19:26:13 +0300

    Organized imports of the authentication succcess handler

--- src/main/java/internship/issuetracker/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/internship/issuetracker/handler/CustomAuthenticationSuccessHandler.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
